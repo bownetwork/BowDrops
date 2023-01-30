943d9e2fb0d0f8d86eddaa0988d1f3a535c50867
bowfun <bowfun@bownet.xyz>
2023-01-30 10:33:13 -0700

    Initial actual commit
    thing

---
+++ src/main/java/com/bownet/drops/ClaimDropCMD.java
@@ -0,0 +1,76 @@
+package com.bownet.drops;
+
+import io.th0rgal.oraxen.api.OraxenItems;
+import io.th0rgal.oraxen.items.ItemBuilder;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import java.util.List;
+
+public class ClaimDropCMD implements CommandExecutor {
+
+    private Main main;
+    public ClaimDropCMD(Main main) {
+        this.main = main;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            String prefix = main.getConfig().getString("Prefix");
+            if (player.hasPermission("bowdrops.claimdrop")) {
+                if (args.length == 1) {
+                    String codeInUse = args[0];
+                    List<String> codes = main.getConfig().getStringList("Codes");
+                    List<String> rewards = main.getConfig().getStringList("Rewards");
+                    if (codes.contains(codeInUse)) {
+                        int codeIndex = codes.indexOf(codeInUse);
+                        String rewardItem = rewards.get(codeIndex);
+                        if (rewardItem.contains("ORAXEN:")) {
+                            String nameOxnItem = rewardItem.replace("ORAXEN:", "");
+                            String nameOxnItem2 = nameOxnItem.toLowerCase();
+                            ItemBuilder OxnUnBuiltItem = OraxenItems.getItemById(nameOxnItem2);
+                            if (OxnUnBuiltItem == null) {
+                                player.sendMessage(prefix + " " + ChatColor.RED + "There was an error, as the item couldn't be found.");
+                                System.out.println("BowDrops: The drop item couldn't be found!");
+                            } else {
+                                ItemStack OxnBuiltItem = OxnUnBuiltItem.build();
+                                player.getInventory().addItem(OxnBuiltItem);
+                                player.sendMessage(prefix + " " + ChatColor.GREEN + "Item has been delivered to your inventory successfully.");
+                            }
+                        } else if (rewardItem.contains("MINECRAFT:")) {
+                            String nameMCItem = rewardItem.replace("MINECRAFT:", "");
+                            Material MCItemName = Material.getMaterial(nameMCItem);
+                            if (MCItemName == null) {
+                                player.sendMessage(prefix + " " + ChatColor.RED + "There was an error, as the item couldn't be found.");
+                                System.out.println("BowDrops: The drop item couldn't be found!");
+                            } else {
+                                ItemStack MCItem = new ItemStack(MCItemName, 1);
+                                player.getInventory().addItem(MCItem);
+                                player.sendMessage(prefix + " " + ChatColor.GREEN + "Item has been delivered to your inventory successfully.");
+                            }
+                        } else {
+                            player.sendMessage(prefix + " " + ChatColor.RED + "There was an error, as the item couldn't be found.");
+                            System.out.println("BowDrops: The drop item couldn't be found!");
+                        }
+                    } else {
+                        player.sendMessage(prefix + " " + ChatColor.RED + "This drop code could not be found.");
+                    }
+                } else {
+                    player.sendMessage(prefix + " " + ChatColor.DARK_AQUA + "Claim Drop Usage:");
+                    player.sendMessage(ChatColor.AQUA + "/claimdrop (drop code): Claim an item drop from a code.");
+                }
+            } else {
+                player.sendMessage(prefix + " " + ChatColor.RED + "You don't have permission to run this command!");
+            }
+        } else {
+            System.out.println("Only players can run this command!");
+        }
+        return false;
+    }
+}
---
+++ src/main/java/com/bownet/drops/Main.java
@@ -0,0 +1,66 @@
+package com.bownet.drops;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.Container;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+        getCommand("chestdrop").setExecutor(new ChestDropCMD(this));
+        getCommand("claimdrop").setExecutor(new ClaimDropCMD(this));
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
+        System.out.println("BowDrops has been loaded.");
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("BowDrops has been unloaded.");
+    }
+
+    @EventHandler
+    public void PlayerInteractEvent(PlayerInteractEvent e) {
+        try {
+            if (e.getClickedBlock().getType() == Material.CHEST) {
+                Container chest = (Container) e.getClickedBlock().getState();
+                ItemStack identifier = chest.getInventory().getItem(0);
+                ItemStack identifierCheck = new ItemStack(Material.ANDESITE, 1);
+                ItemMeta metaIdentifier = identifierCheck.getItemMeta();
+                metaIdentifier.setDisplayName(ChatColor.DARK_AQUA + "BowDrops Identifier");
+                identifierCheck.setItemMeta(metaIdentifier);
+                if (identifier.isSimilar(identifierCheck)) {
+                    String prefix = getConfig().getString("Prefix");
+                    e.setCancelled(true);
+                    ItemStack itemToGive = chest.getInventory().getItem(13);
+                    e.getPlayer().getInventory().addItem(itemToGive);
+                    chest.getInventory().clear();
+                    e.getClickedBlock().setType(Material.AIR);
+                    for (Entity entity : chest.getLocation().getChunk().getEntities()) {
+                        if (entity.getLocation().distance(chest.getLocation()) <= 1.5) {
+                            if (entity.getType() == EntityType.ARMOR_STAND) {
+                                entity.remove();
+                            }
+                        }
+                    }
+                    e.getPlayer().sendMessage(prefix + " " + ChatColor.GREEN + "You successfully claimed the drop!");
+                }
+            }
+        } catch (NullPointerException exception) {
+        }
+    }
+
+
+}
